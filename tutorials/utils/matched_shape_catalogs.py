1a7021753b5bd6c23c1b12304851997bc8e594e4
Melanie Simet <melanie.simet@gmail.com>
2019-02-27 20:42:19 -0800

    Remove filter_ from some shape measurement calls; rename utils file

--- tutorials/utils/matched_shape_catalogs.py
+++ tutorials/utils/matched_shape_catalogs.py
@@ -24,109 +24,7 @@ from lsst.sims.utils import cartesianFromSpherical, sphericalFromCartesian
 import GCRCatalogs
 from GCR import GCRQuery
 from desc_dc2_dm_data import REPOS
-
-# Class from Jim Chiang
-class FieldRotator(object):
-
-    def __init__(self, ra0, dec0, ra1, dec1):
-        """
-        Parameters
-        ----------
-        ra0, dec0 are the coordinates of the original field
-        center in degrees
-        ra1, dec1 are the coordinates of the new field center
-        in degrees
-        The transform() method of this class operates by first
-        applying a rotation that carries the original field center
-        into the new field center.  Points are then transformed into
-        a basis in which the unit vector defining the new field center
-        is the x-axis.  A rotation about the x-axis is applied so that
-        a point that was due north of the original field center is still
-        due north of the field center at the new location.  Finally,
-        points are transformed back into the original x,y,z bases.
-        """
-
-        # find the rotation that carries the original field center
-        # to the new field center
-        xyz = cartesianFromSpherical(np.radians(ra0), np.radians(dec0))
-        xyz1 = cartesianFromSpherical(np.radians(ra1), np.radians(dec1))
-        if np.abs(1.0-np.dot(xyz, xyz1))<1.0e-10:
-            self._transformation = np.identity(3, dtype=float)
-            return
-
-        first_rotation = rotationMatrixFromVectors(xyz, xyz1)
-
-        # create a basis set in which the unit vector
-        # defining the new field center is the x axis
-        xx = np.dot(first_rotation, xyz)
-        rng = np.random.RandomState(99)
-        mag = np.NaN
-        while np.abs(mag)<1.0e-20 or np.isnan(mag):
-            random_vec = rng.random_sample(3)
-            comp = np.dot(random_vec, xx)
-            yy = random_vec - comp*xx
-            mag = np.sqrt((yy**2).sum())
-            yy /= mag
-
-        zz = np.cross(xx, yy)
-
-        to_self_bases = np.array([xx,
-                                  yy,
-                                  zz])
-
-        out_of_self_bases =to_self_bases.transpose()
-
-        # Take a point due north of the original field
-        # center.  Apply first_rotation to carry it to
-        # the new field.  Transform it to the [xx, yy, zz]
-        # bases and find the rotation about xx that will
-        # make it due north of the new field center.
-        # Finally, transform back to the original bases.
-        d_dec = 0.1
-        north = cartesianFromSpherical(np.radians(ra0),
-                                       np.radians(dec0+d_dec))
-
-        north = np.dot(first_rotation, north)
-
-        north_true = cartesianFromSpherical(np.radians(ra1),
-                                            np.radians(dec1+d_dec))
-
-        north = np.dot(to_self_bases, north)
-        north_true = np.dot(to_self_bases, north_true)
-        north = np.array([north[1], north[2]])
-        north /= np.sqrt((north**2).sum())
-        north_true = np.array([north_true[1], north_true[2]])
-        north_true /= np.sqrt((north_true**2).sum())
-
-        c = north_true[0]*north[0]+north_true[1]*north[1]
-        s = north[0]*north_true[1]-north[1]*north_true[0]
-        norm = np.sqrt(c*c+s*s)
-        c = c/norm
-        s = s/norm
-
-        nprime = np.array([c*north[0]-s*north[1],
-                           s*north[0]+c*north[1]])
-
-        yz_rotation = np.array([[1.0, 0.0, 0.0],
-                                [0.0, c, -s],
-                                [0.0, s, c]])
-
-        second_rotation = np.dot(out_of_self_bases,
-                                 np.dot(yz_rotation,
-                                        to_self_bases))
-
-        self._transformation = np.dot(second_rotation,
-                                      first_rotation)
-
-    def transform(self, ra, dec):
-        """
-        ra, dec are in degrees; return the RA, Dec coordinates
-        of the point about the new field center
-        """
-        xyz = cartesianFromSpherical(np.radians(ra), np.radians(dec)).transpose()
-        xyz = np.dot(self._transformation, xyz).transpose()
-        ra_out, dec_out = sphericalFromCartesian(xyz)
-        return np.degrees(ra_out), np.degrees(dec_out)
+from fieldRotator import FieldRotator
 
 class RegionSelector:
     """
@@ -228,6 +126,8 @@ class ShearRotator:
             The number of the desired tract
         patchId: str
             "%i,%i"-formatted string indicating the desired patch
+        filter_: str
+            The filter to analyze
         convert_to_sky_coords: bool
             Whether to rotate to sky coordinates or leave in native coordinates (default: True)
         cache_size: int
@@ -242,9 +142,7 @@ class ShearRotator:
             self.wcs = skymap[tractId].getWcs()
         else:
             self.wcs = None
-        self.saved_transforms = None
-        self.saved_moments = None
-        self.saved_covariances = None
+        self.clear()
         
     def clear(self):
         self.saved_transforms = None
@@ -314,7 +212,7 @@ class ShearRotator:
         return np.sqrt(dg1_dixx**2 * cov_ixx + dg1_diyy**2 * cov_iyy)
 
     def g2_err(self, ra, dec, ixx, iyy, ixy, cov_ixx, cov_iyy, cov_ixy):
-        ixx, iyy, ixy = self.getRotatedMoments(ra, dec, ixx, iyy, ixy, filter_)
+        ixx, iyy, ixy = self.getRotatedMoments(ra, dec, ixx, iyy, ixy)
         cov_ixx, cov_iyy, cov_ixy = self.getRotatedCovariance(ra, dec, cov_ixx, cov_iyy, cov_ixy)
         dg2_dixx = -2.*ixy/(ixx+iyy)**2
         dg2_diyy = -2.*ixy/(ixx+iyy)**2
@@ -323,8 +221,9 @@ class ShearRotator:
                             dg2_dixy**2 * cov_ixy)
 
     def sigma_err(self, ra, dec, ixx, iyy, ixy, cov_ixx, cov_iyy, cov_ixy):
-        ixx, iyy, ixy = self.getRotatedMoments(ra, dec, ixx, iyy, ixy, filter_)
+        ixx, iyy, ixy = self.getRotatedMoments(ra, dec, ixx, iyy, ixy)
         cov_ixx, cov_iyy, cov_ixy = self.getRotatedCovariance(ra, dec, cov_ixx, cov_iyy, cov_ixy)
+        sigma = self.sigma(ra, dec, ixx, iyy, ixy)
         dsigma_dixx = 0.25/sigma**3*iyy
         dsigma_diyy = 0.25/sigma**3*ixx
         dsigma_dixy = -0.5/sigma**3*ixy
@@ -536,3 +435,4 @@ def get_catalogs(tract=4851, filter_='i', mag_max=24.5,
     star_catalog = {k: star_catalog[k][star_mask] for k in star_catalog}
 
     return galaxy_catalog, star_catalog, egc_catalog, truth_catalog
+
